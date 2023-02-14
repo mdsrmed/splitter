ec041b3a5bc9f2d9f1cea109e3cf72da03aa02ac
mdsrmed <mdsrmed@gmail.com>
2023-02-14 18:02:00 -0500

    Initial Commit

---
+++ splitter/splitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  splitterApp.swift
+//  splitter
+//
+//  Created by Md Shohidur Rahman on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct splitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
